1e79888d1c9012abcb1e8fa3412322f3e5d3f9ba
Marko Golovko <markgolovko@gmail.com>
2024-09-23 09:56:37 +0200

    adjust main page

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,34 +1,34 @@
 export default {
   top: [
+    // {
+    //   text: 'About',
+    //   url: '/about/'
+    // },
+    // {
+    //   text: 'Docs',
+    //   url: '/get-started/'
+    // },
+    // {
+    //   text: 'Built with',
+    //   url: '/built-with/'
+    // },
     {
-      text: 'About',
-      url: '/about/'
-    },
-    {
-      text: 'Docs',
-      url: '/get-started/'
-    },
-    {
-      text: 'Built with',
-      url: '/built-with/'
-    },
-    {
-      text: 'Blog',
+      text: 'Thoughts',
       url: '/blog/'
     }
   ],
   bottom: [
-    {
-      text: 'Style guide',
-      url: '/styleguide/'
-    },
-    {
-      text: 'Imprint',
-      url: '/imprint/'
-    },
-    {
-      text: 'Privacy',
-      url: '/privacy/'
-    }
+    // {
+    //   text: 'Style guide',
+    //   url: '/styleguide/'
+    // },
+    // {
+    //   text: 'Imprint',
+    //   url: '/imprint/'
+    // },
+    // {
+    //   text: 'Privacy',
+    //   url: '/privacy/'
+    // }
   ]
 };
